bd8c67beb7fbbada5e1403b483c8c06e115ec64d
Paulina <paulinaszulc23@interia.pl>
2023-05-30 08:57:27 +0200

    progress na zajeciach, dodane sumy, avg itd; poprawic wczytywanie z pliku

--- Spreadsheet.cpp
+++ Spreadsheet.cpp
@@ -98,6 +98,110 @@ string Spreadsheet::getCellType(int row, int col) const {
     return "Cell";
 }
 
+float Spreadsheet::Sum(int rowmin, int rowmax, int colmin, int colmax) const {
+    float sum = 0.0f;
+    for (int i=rowmin; i<=rowmax; i++) {
+        for (int j=colmin; j<=colmax; j++) {
+            sum += cells.getValueAsFloat(i, j);
+        }
+    }
+    return sum;
+}
+
+float Spreadsheet::Average(int rowmin, int rowmax, int colmin, int colmax) const {
+    float sum = 0.0f;
+    int count = 0;
+    for (int i=rowmin; i<=rowmax; i++) {
+        for (int j=colmin; j<=colmax; j++) {
+            sum += cells.getValueAsFloat(i, j);
+            count++;
+        }
+    }
+    return sum / count;
+}
+
+float Spreadsheet::Max(int rowmin, int rowmax, int colmin, int colmax) const {
+    float max = cells.getValueAsFloat(rowmin, colmin);
+    rowmin++;
+    colmin++;
+    for (int i=rowmin; i<=rowmax; i++) {
+        for (int j=colmin; j<=colmax; j++) {
+            if (cells.getValueAsFloat(i, j) > max) {
+                max = cells.getValueAsFloat(i, j);
+            }
+        }
+    }
+    return max;
+}
+
+float Spreadsheet::Min(int rowmin, int rowmax, int colmin, int colmax) const {
+    float min = cells.getValueAsFloat(rowmin, colmin);
+    rowmin++;
+    colmin++;
+    for (int i=rowmin; i<=rowmax; i++) {
+        for (int j=colmin; j<=colmax; j++) {
+            if (cells.getValueAsFloat(i, j) < min) {
+                min = cells.getValueAsFloat(i, j);
+            }
+        }
+    }
+    return min;
+}
+
+string Spreadsheet::Concat (int rowmin, int rowmax, int colmin, int colmax) const {
+    string concat = "";
+    for (int i=rowmin; i<=rowmax; i++) {
+        for (int j=colmin; j<=colmax; j++) {
+            concat += cells.getValueAsString(i, j);
+        }
+    }
+    return concat;
+}
+
+void Spreadsheet::Save (string filename) const {
+    ofstream file;
+    file.open(filename);
+    file << cells.size() << ";" << cells[0].size() << endl;
+
+    for (int i=0; i<cells.size(); i++) {
+        for (int j=0; j<cells[i].size(); j++) {
+            file << i << ";" << j << ";" << cells.getCellType << ";" << cells.getValueAsString(i, j) << ";" << endl;
+        }
+    }
+    file.close();
+
+}
+
+void Spreadsheet::Load (string filename) {
+    ifstream file;
+    file.open(filename);
+    string line;
+    getline(file, line);
+    int rows = stoi(line.substr(0, line.find(";")));
+    int cols = stoi(line.substr(line.find(";") + 1, line.length()));
+    cells.resize(rows);
+    for (int i=0; i<rows; i++) {
+        cells[i].resize(cols, nullptr);
+    }
+    while (getline(file, line)) {
+        int row = stoi(line.substr(0, line.find(";")));
+        line = line.substr(line.find(";") + 1, line.length());
+        int col = stoi(line.substr(0, line.find(";")));
+        line = line.substr(line.find(";") + 1, line.length());
+        string type = line.substr(0, line.find(";"));
+        line = line.substr(line.find(";") + 1, line.length());
+        string value = line.substr(0, line.find(";"));//to poprawic 
+        if (type == "NumericCell") {
+            cells[row][col] = new NumericCell();
+        } else {
+            cells[row][col] = new StringCell();
+        }
+        cells[row][col]->setValue(value);
+    }
+    file.close();
+
+}
+
 Spreadsheet::~Spreadsheet()
 {
     for (auto &row : cells)
